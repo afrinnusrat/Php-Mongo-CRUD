68c12d2160dbda9b97fd1734f8354254082629d0
Avnish alok <avnishalok.se@gmail.com>
2016-12-27 02:20:24 +0530

    Add files via upload

--- db.inc.php
+++ db.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once 'mongodb-lib/vendor/autoload.php';
 $databasename = (new MongoDB\Client)->login_db; // or new MongoDB\Client("mongodb://localhost:27017")->example;
 ?>
